1451e741c0c2208daa4f8ab98d7e919ee16a8937
prakashjoshi <prakashchandrajoshi2209@gmail.com>
2025-05-01 19:47:44 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running');
+});
+
