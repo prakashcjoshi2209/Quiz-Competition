826074de550795159c40006f4f04b5777d8d14a6
prakashjoshi <prakashchandrajoshi2209@gmail.com>
2025-05-06 15:24:03 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,23 @@ const app = express();
 // Middlewares
 // app.use(cors());
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://quiz-competition-five.vercel.app'
+];
+
 app.use(cors({
-  origin: 'https://quiz-competition-five.vercel.app', // Frontend URL
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
+
 
 app.use(express.json());
 
